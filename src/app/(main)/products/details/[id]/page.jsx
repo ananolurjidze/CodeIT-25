ed5c3c68a40ebc059dc5a71399c22d018a3cb7e6
aannanno <ananolurjidze3@gmail.com>
2025-04-28 17:27:04 +0400

    details added

--- src/app/(main)/products/details/[id]/page.jsx
+++ src/app/(main)/products/details/[id]/page.jsx
@@ -1,7 +1,32 @@
-const page = async ({ params }) => {
-  const { id } = await params;
+import styles from "./page.module.css"
+async function page({ params }) {
+    const id = params.id;
+  
+    let products;
+    try {
+      const data = await fetch('https://fakestoreapi.com/products');
+      products = await data.json();
+    } catch (error) {
+      throw Error(error);
+    }
 
-  return <div>{id}</div>;
-};
-
-export default page;
+    const product = products.find((p) => p.id == id);
+  
+  
+    return (
+      <main className={styles.container}>
+        <div>
+          <p className={styles.Id}>Product ID: {product.id}</p>
+          <h1 className={styles.title}>Product Title: {product.title}</h1>
+          <img className={styles.img} src={product.image} alt={product.title}  />
+          <p className={styles.description}>Description: {product.description}</p>
+          <p className={styles.category}>Category: {product.category}</p>
+          <p className={styles.price}>Price: ${product.price}</p>
+          <p className={styles.Count}>Count: {product.rating.count}</p>
+          <p className={styles.rate}>Rating: {product.rating.rate}</p>
+        </div>
+      </main>
+    );
+  }
+  
+  export default page;
--- src/app/(main)/products/page.jsx
+++ src/app/(main)/products/page.jsx
@@ -1,25 +1,28 @@
 import ProductItem from "@/components/ProductItem/ProductItem";
-import styles from "./page.module.css";
-import Link from "next/link";
+import error from "./error";
+import styles from "./page.module.css"
+import Link from 'next/link';
 
-const Products = async () => {
-  let products;
-  try {
-    const data = await fetch("https://fakestoreapi.com/products");
-    products = await data.json();
-  } catch (error) {
-    throw Error(error);
-  }
+const Profile = async () => {
+   let products;
+   try {
+    const data = await fetch('https://fakestoreapi.com/products')
+    products= await data.json();
+   } catch(error){
+    throw Error(error)
+   }
+   console.log(products)
 
-  return (
-    <div className={styles.container}>
-      {products.map((product) => (
-        <Link key={product.id} href={`/products/details/${product.id}`}>
-          <ProductItem item={product} />
-        </Link>
-      ))}
-    </div>
-  );
-};
 
-export default Products;
+    return (
+        <div className={styles.container}>
+            {products.map((product) => (
+                <Link key={product.id} href={`/products/details/${product.id}`}>
+                   <ProductItem key={product.id} item={product}/>
+           </Link>
+         ))}
+        </div>
+    )
+}
+
+export default Profile;
